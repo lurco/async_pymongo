8f8f030e5a0af7a2d3c8b276494ebdadcaa2cede
Andrzej Legutko <andrzejlegutko@gmail.com>
2025-05-29 01:34:37 +0200

    add username, password and db_name for envs + default.env

--- config/config.py
+++ config/config.py
@@ -4,13 +4,13 @@ from dotenv import load_dotenv
 from pydantic_settings import BaseSettings
 
 load_dotenv()
-CONNECTION_STRING = os.getenv("CONNECTION_STRING")
-if CONNECTION_STRING is None:
-    raise ValueError("Error loading CONNECTION_STRING environmental variable")
+CONNECTION_STRING = os.getenv("CONNECTION_STRING", "mongodb://localhost:27017/")
+DB_NAME = os.getenv("MONGO_DB_NAME", "mydatabase")
 
 
 class Settings(BaseSettings):
     MONGO_CONNECTION_STRING: str
+    MONGO_DB_NAME: str
 
     def __repr__(self) -> str:
         """Return a string representation of the model."""
@@ -21,4 +21,4 @@ class Settings(BaseSettings):
         return f"{self.__class__.__name__}({', '.join(attrs)})"
 
 
-settings = Settings(MONGO_CONNECTION_STRING=CONNECTION_STRING)
+settings = Settings(MONGO_CONNECTION_STRING=CONNECTION_STRING, MONGO_DB_NAME=DB_NAME)
